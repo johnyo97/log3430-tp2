5c32d6ae1e99637149d5dc79fec603b98af76dcb
johnyo97 <jonathanoneill1997@gmail.com>
2019-09-24 23:01:57 -0400

    JON: Added a few tests, might merge to master later

--- test_generators.py
+++ test_generators.py
@@ -11,6 +11,42 @@ class MyTestCase(unittest.TestCase):
     def setUp(self):
         self.simpleGraph = simple(4, 4)
 
+    def test_when_too_fee_edges_was_raised(self):
+        graph = None
+        exceptionWasRaised = False
+
+        try:
+            graph = simple(4,-1)
+        except ValueError:
+            exceptionWasRaised = True
+
+        self.assertTrue(exceptionWasRaised, 'Exception was not raised')
+
+
+    def test_when_raise_too_many_edges_was_raised(self):
+        graph = None
+        exceptionWasRaised = False
+
+        try:
+            graph = simple(4, 8)
+        except ValueError:
+            exceptionWasRaised = True
+
+        self.assertTrue(exceptionWasRaised, 'Exception was not raised')
+
+    def test_simple_graph_has_no_multiple_edges(self):
+        edges = self.simpleGraph.edges()
+
+        values = []
+        count = 0
+        for edge in edges:
+            if edge not in values:
+                values.append(edge)
+            else:
+                count = count + 1
+
+        self.assertTrue(count == 0)
+
     def test_simple_graph_has_no_loops(self):
         edges = self.simpleGraph.edges()
 
