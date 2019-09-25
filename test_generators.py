75677184975b8c4bdd171c735e57491d1cacd7da
johnyo97 <jonathanoneill1997@gmail.com>
2019-09-24 23:04:44 -0400

    JON: Removed test, should be black box testing

--- test_generators.py
+++ test_generators.py
@@ -11,29 +11,6 @@ class MyTestCase(unittest.TestCase):
     def setUp(self):
         self.simpleGraph = simple(4, 4)
 
-    def test_when_too_fee_edges_was_raised(self):
-        graph = None
-        exceptionWasRaised = False
-
-        try:
-            graph = simple(4,-1)
-        except ValueError:
-            exceptionWasRaised = True
-
-        self.assertTrue(exceptionWasRaised, 'Exception was not raised')
-
-
-    def test_when_raise_too_many_edges_was_raised(self):
-        graph = None
-        exceptionWasRaised = False
-
-        try:
-            graph = simple(4, 8)
-        except ValueError:
-            exceptionWasRaised = True
-
-        self.assertTrue(exceptionWasRaised, 'Exception was not raised')
-
     def test_simple_graph_has_no_multiple_edges(self):
         edges = self.simpleGraph.edges()
 
