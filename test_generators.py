40c9f35e3e6d6d840473c937089960a116099255
johnyo97 <jonathanoneill1997@gmail.com>
2019-09-24 22:43:34 -0400

    JON: Loop test was somehow lost ? Adding it again

--- test_generators.py
+++ test_generators.py
@@ -1,7 +1,29 @@
 import unittest
+import re
+
+from generators import simple
+from models import Graph
 
 
 class MyTestCase(unittest.TestCase):
+
+
+    def setUp(self):
+        self.simpleGraph = simple(4, 4)
+
+    def test_simple_graph_has_no_loops(self):
+        edges = self.simpleGraph.edges()
+
+        count = 0;
+        for edge in edges:
+            values = []
+            for vertice in edge:
+                if vertice not in values:
+                    values.append(vertice)
+                else:
+                   count = count + 1
+
+        self.assertTrue(count == 0)
 
 
 
